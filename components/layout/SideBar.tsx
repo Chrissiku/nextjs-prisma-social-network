298759c56a83ca8ff17acff94db67803a6ba799c
Chris Siku <chrissiku5@gmail.com>
2023-05-11 21:52:52 +0200

    Create Button components

--- components/layout/SideBar.tsx
+++ components/layout/SideBar.tsx
@@ -13,21 +13,19 @@ const SideBar = () => {
     { label: "Profile", href: "/users/123", icon: FaUser },
   ];
   return (
-    <div className="col-span-1 h-full pr-4 md:pr-6">
-      <div className="flex flex-col items-end">
-        <div className="space-y-2 lg:-[230px]">
-          <SideBarLogo />
-          {items.map((item) => (
-            <SideBarItem
-              key={item.href}
-              href={item.href}
-              label={item.label}
-              icon={item.icon}
-            />
-          ))}
-          <SideBarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
-          <SideBarTweetButton />
-        </div>
+    <div className="flex flex-col items-end">
+      <div className="space-y-2 lg:-[230px]">
+        <SideBarLogo />
+        {items.map((item) => (
+          <SideBarItem
+            key={item.href}
+            href={item.href}
+            label={item.label}
+            icon={item.icon}
+          />
+        ))}
+        <SideBarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+        <SideBarTweetButton />
       </div>
     </div>
   );
